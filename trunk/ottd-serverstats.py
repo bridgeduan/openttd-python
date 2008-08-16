7a36e10d3bc06160a73b5f74c4fdeaca1b66c6c6
yorickvanpelt <yorickvanpelt@788b6e31-7352-0410-afda-7f0ae6d29507>
2008-08-16 12:36:03 +0000

    - Fix[serverstats]: verbose mode was broken - Codechange[serverstats]: replace some counters with counters["*"] variables - Add[serverstats]: extend the grf database

--- trunk/ottd-serverstats.py
+++ trunk/ottd-serverstats.py
@@ -79,7 +79,7 @@ class Grf(DataStorageClass):
 class ClientGameInfo(Client):
 	# this class implements the thread start method
 	def run(self):
-		self.connect()
+		self.connect(M_UDP)
 		if len(self.errors) == 0:
 			self.socket_udp.settimeout(10)
 			SERVERS[self.uid] = self.getGameInfo()
@@ -105,10 +105,8 @@ def main():
 	
 	counter = 0
 	for server in servers:
-		ip = server[0]
-		port = server[1]
 		counter += 1
-		client = ClientGameInfo(ip, port, False, counter)
+		client = ClientGameInfo(server[0], server[1], False, counter)
 		client.start()
 	
 	t = time.time()
@@ -121,7 +119,6 @@ def main():
 				ln = len(SERVERS.keys())
 	
 	counters={}
-	sumcounter = 0
 	servererr=0
 	counters["server_revision"] = {} # server revision
 	counters["server_lang"] = {} # language
@@ -133,7 +130,6 @@ def main():
 	myottdservers = 0
 	newgrf_servers = 0
 	newgrf_clients = 0
-	clients_overall = 0
 	
 	for k in SERVERS.keys():
 		if type(SERVERS[k]) == type(""):
@@ -143,12 +139,15 @@ def main():
 			continue
 		if not SERVERS[k] is None:
 			server = SERVERS[k]
-			if VERBOSE:
-				print "%3d: %s"%(k,server.__str__())
-			for i in ["game_date","start_date","companies_max","companies_on","spectators_max","use_password","clients_max","clients_on","spectators_on","dedicated"]:
+			#if VERBOSE:
+			#	print "%3d: %s"%(k,server.__str__())
+			for i in ["server_count","game_date","start_date","companies_max","companies_on","spectators_max","use_password","clients_max","clients_on","spectators_on","dedicated"]:
 				if not i in counters.keys():
 					counters[i] = 0
-				counters[i] += getattr(server, i)
+				if i == "server_count":
+					counters[i] += 1
+				else:
+					counters[i] += getattr(server, i)
 			
 			# handle list fields
 			for i in ["server_revision","server_lang","map_name","map_size","map_set"]:
@@ -183,7 +182,8 @@ def main():
 					used_grfs[grfname].name = GRFS.getgrfname(grf[1])
 				used_grfs[grfname].addServer(server)
 				grfcount += 1
-			
+				
+			# calculate amount of newgrf servers
 			if len(grfs) > 0:
 				valid = True
 				if len(grfs) == 1:
@@ -192,34 +192,34 @@ def main():
 				if valid:
 					newgrf_servers += 1
 					newgrf_clients += server.clients_on
-			clients_overall += server.clients_on
-			sumcounter+=1
-	GRFS.savetofile("newgrfs.grflist")
-	def percent(value1, value2=100):
+			
+		# save the grf list if it is changed
+		GRFS.savetofile("newgrfs.grflist")
+	def percent(value1, value2=counters["server_count"]):
 		return float(value1)/float(value2)*100
 	if VERBOSE:
-		print '#'*80
+		print '#'*79
 	print "OpenTTD Server statistics (%s):" % time.ctime()
 	print "responding servers: %d, not responding: %d" % (len(SERVERS.keys()) - servererr, servererr)
 	print "companies: %d / %d (%.2f%%)" % (counters["companies_on"], counters["companies_max"], percent(counters["companies_on"], counters["companies_max"]))
 	print "spectators: %d / %d (%.2f%%)" % (counters["spectators_on"], counters["spectators_max"], percent(counters["spectators_on"], counters["spectators_max"]))
 	print "clients: %d / %d (%.2f%%)" % (counters["clients_on"], counters["clients_max"], percent(counters["clients_on"], counters["clients_max"]))
-	print "password protected servers: %d / %d (%.2f %%)" % (counters["use_password"], sumcounter, percent(counters["use_password"], sumcounter))
-	print "dedicated servers: %d / %d, (%.2f %%)" % (counters["dedicated"], sumcounter, percent(counters["dedicated"], sumcounter))
-	print "newGRF servers: %d / %d (%.2f%%)" % (newgrf_servers, sumcounter, percent(newgrf_servers, sumcounter))
-	print "players on newGRF servers: %d / %d (%.2f%%)" % (newgrf_clients, clients_overall, percent(newgrf_clients, clients_overall))
-	print "myottd.net servers online: %d" % (myottdservers)
+	print "password protected servers: %d / %d (%.2f %%)" % (counters["use_password"], counters["server_count"], percent(counters["use_password"]))
+	print "dedicated servers: %d / %d, (%.2f %%)" % (counters["dedicated"], counters["server_count"], percent(counters["dedicated"]))
+	print "newGRF servers: %d / %d (%.2f%%)" % (newgrf_servers, counters["server_count"], percent(newgrf_servers))
+	print "players on newGRF servers: %d / %d (%.2f%%)" % (newgrf_clients, counters["clients_on"], percent(newgrf_clients, counters["clients_on"]))
+	print "myottd.net servers online: %d / %d (%.2f%%)" % (myottdservers, counters["server_count"], percent(myottdservers))
 	
 	
 	print ""
 	print "used versions:"
 	for item in sorted(counters["server_revision"].items(), key=itemgetter(1), reverse=True):
-		print " % 16s: %3d (% 5.1f%%), %3d clients" % (item[0], item[1][0], percent(item[1][0], sumcounter), item[1][1])
+		print " % 16s: %3d (% 5.1f%%), %3d clients" % (item[0], item[1][0], percent(item[1][0]), item[1][1])
 	
 	print ""
 	print "used map sizes:"
 	for item in sorted(counters["map_size"].items(), key=itemgetter(1), reverse=True):
-		print " % 20s: %3d (% 5.1f%%), %3d clients" % (item[0], item[1][0], percent(item[1][0], sumcounter), item[1][1])
+		print " % 20s: %3d (% 5.1f%%), %3d clients" % (item[0], item[1][0], percent(item[1][0]), item[1][1])
 		
 	print ""
 	print "used languages:"
@@ -228,7 +228,7 @@ def main():
 			langstr = known_languages[item[0]]
 		else:
 			langstr = "unkown language: %d" % item[0]
-		print " % 20s: %3d (% 5.1f%%), %3d clients" % (langstr, item[1][0], percent(item[1][0], sumcounter), item[1][1])
+		print " % 20s: %3d (% 5.1f%%), %3d clients" % (langstr, item[1][0], percent(item[1][0]), item[1][1])
 	
 	print ""
 	print "used landscapes:"
@@ -238,12 +238,12 @@ def main():
 			lcs = landscapes[item[0]]
 		else:
 			lcs = "unkown landscape: %d" % item[0]
-		print " % 20s: %3d (% 5.1f%%), %3d clients" % (lcs, item[1][0], percent(item[1][0], sumcounter), item[1][1])
+		print " % 20s: %3d (% 5.1f%%), %3d clients" % (lcs, item[1][0], percent(item[1][0]), item[1][1])
 		
 	print ""
 	print "used maps:"
 	for item in sorted(counters["map_name"].items(), key=itemgetter(1), reverse=True):
-		print " % 50s: %3d (% 5.1f%%), %3d clients" % (item[0], item[1][0], percent(item[1][0], sumcounter), item[1][1])
+		print " % 50s: %3d (% 5.1f%%), %3d clients" % (item[0], item[1][0], percent(item[1][0]), item[1][1])
 
 	print ""
 	print "used GRFs (%d used, %d unique known):" % (grfcount, len(used_grfs))
