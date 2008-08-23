87a75d13702d662f0b65bd54961c47279d5265f9
yorickvanpelt <yorickvanpelt@788b6e31-7352-0410-afda-7f0ae6d29507>
2008-08-23 15:46:59 +0000

    - Codechange: rewrite the part that gets some info from the parent-event

--- ottd_client_event.py
+++ ottd_client_event.py
@@ -24,17 +24,19 @@ class IngameChat(Event):
     """
     dispatchTo = ["sendToIRC", "sendToLog", "sendToCmdProc"]
     def __init__(self, msg, clientid=-1, parent=None, parentclient=None, type=None):
-        if parentclient is None and not parent is None:
-            self.parentclient = parent.parentclient
-        else:
-            self.parentclient = parentclient
-        self.parent = parent
-        if type is None and not self.parent is None and not self.parent.type is None:
-            self.type = self.parent.type
-        else:
             self.type = type
-        if clientid == -1:
-            clientid = self.parentclient.client_id
+            self.parentclient = parentclient
+            self.parent = parent
+        if not parent is None:
+            # get stuff from parents
+            if type is None and not self.parent.type is None:
+                self.type = self.parent.type
+            if parentclient is None and not self.parent.parentclient is None:
+                self.parentclient = self.parent.parentclient
+            if clientid == -1 and not self.parent.clientid == -1:
+                clientid = self.parent.clientid
+            elif clientid == -1 and not self.parentclient is None:
+                clientid = self.parentclient.client_id
         self.playerid = clientid
         self.msg = msg
         if clientid in self.parentclient.playerlist:
