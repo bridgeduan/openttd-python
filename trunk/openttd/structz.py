3365b7945b9ead8ccc2f2a16cfa5cd817efcc3af
yorickvanpelt <yorickvanpelt@788b6e31-7352-0410-afda-7f0ae6d29507>
2008-10-27 20:23:18 +0000

    - Fix: add some additional debugging to the error unpack will give if it feels unpack_from was supposed to be called

--- trunk/openttd/structz.py
+++ trunk/openttd/structz.py
@@ -118,7 +118,7 @@ def unpack(fmt, str):
                 offs += offplus
                 unp.append(unpplus)
         if not offs is len(str):
-            raise Exception("unpack requires string that matches format size, try to use unpack_from")
+            raise Exception("unpack requires string that matches format size, try to use unpack_from, (fmt: %s, str: %s" % (fmt, str.encode('hex')))
         return tuple(unp)
 def pack(fmt, *argv):
     """
