ac718b6199d48c066dd211f7ac14ea18a4a82584
yorickvanpelt <yorickvanpelt@788b6e31-7352-0410-afda-7f0ae6d29507>
2009-10-25 17:10:13 +0000

    - shuffle constants, add md5sum to content_client, add networking protocol to make networking communications easier(not used) and add send_raw method to DataPacket - update newgrfs database

--- trunk/openttd/networking.py
+++ trunk/openttd/networking.py
@@ -1,6 +1,7 @@
 """ stuff that can be used for both client and server """
 import struct
 import constants as const
+from packet import DataPacket
 
 def hash_company_password(password, server_unique_id, game_seed):
     """
@@ -51,3 +52,76 @@ def parsePacketHeader(header):
     @returns:      (size, packettype)
     """
     return const.HEADER.unpack_from(header)
+
+def complete_data_to_packet(data):
+    size, command = parsePacketHeader(data)
+    size -= const.HEADER_SIZE
+    return DataPacket(size, command, data[const.HEADER_SIZE:])
+def packet_to_complete_data(p):
+    header = createPacketHeader(p.command, p.data)
+    return header + p.data
+    
+class OTTDProtocol:
+    """
+    Protocol description for OpenTTD-python
+    When subclassing, overwrite sendString(self, msg) for sending raw data, and overwrite packetReceived for processing incoming data
+    @ivar recvd: internal incoming data buffer
+    @type recvd: string
+    """
+    def stringReceived(self, msg):
+        """
+        Call this method whenever you want to process data. it calls the dataReceived method every time a whole packet is available
+        @param msg: received data
+        @type  msg: string
+        """
+        try:
+            self.recvd += msg 
+        except AttributeError:
+            self.recvd = msg
+        
+        while len(self.recvd) >= const.PACKETSIZEHEADER_SIZE:
+            size, = const.PACKETSIZEHEADER.unpack_from(self.recvd)
+            if len(self.recvd) < size:
+                break
+            packet = self.recvd[:size]
+            self.recvd = self.recvd[size:]
+            self.dataReceived(packet)
+    def dataReceived(self, data):
+        """
+        Call this method for whole packets in their raw form. It converts it into a DataPacket and calls PacketReceived with it.
+        @param data: data to convert
+        @type  data: string
+        """
+        self.packetReceived(complete_data_to_packet(data))
+    def packetReceived(self, packet):
+        """
+        Overwrite this method to do something with the packet it.
+        @param packet: the packet that was received
+        @type  packet: DataPacket instance
+        """
+        pass #overwrite
+    def sendString(self, msg):
+        """
+        Overwrite this method to send raw data somewhere, the extra arguments from sendData and sendPacket also get passed to here
+        @param msg: the data to send
+        @type  msg: string
+        """
+        pass #overwrite sending raw data
+    def sendData(self, data, command, *arg):
+        """
+        convert data+command to raw data and call sendString with it, also pass other arguments to sendString
+        @param    data: packet payload
+        @type     data: string
+        @param command: packet id for use in the header
+        @type  command: int
+        """
+        self.sendString(createPacketHeader(command, data) + data, *arg)
+    def sendPacket(self, packet, *arg):
+        """
+        convert DataPacket into payload and packetid and pass it to sendData, also passes optional arguments to sendData which passes it to sendString
+        @param packet: packet to convert
+        @type  packet: DataPacket instance
+        """
+        self.sendData(packet.data, packet.command, *arg)
+    
+    
