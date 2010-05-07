f4be4471220c3e3d4139a0eb0d77e4091a7209e6
yorickvanpelt <yorickvanpelt@788b6e31-7352-0410-afda-7f0ae6d29507>
2010-05-07 15:13:30 +0000

    - Change: update content client

--- openttd/content_client.py
+++ openttd/content_client.py
@@ -7,7 +7,25 @@ from log import LOG
 import socket
 from datastorageclass import DataStorageClass
 import structz
-
+import version
+import httplib
+import _error
+class MirrorClient(httplib.HTTPConnection):
+    def __init__(self):
+        httplib.HTTPConnection.__init__(self, const.NETWORK_CONTENT_MIRROR_HOST, const.NETWORK_CONTENT_MIRROR_PORT)
+    def connect(self):
+        httplib.HTTPConnection.connect(self)
+    def disconnect(self):
+        httplib.HTTPConnection.close(self)
+    def ContentList_URL_Get(self, ids):
+        req = "\n".join([str(id) for id in ids]) + "\n"
+        self.request("POST", const.NETWORK_CONTENT_MIRROR_URL, req, {'Content-Type': 'text/plain', 'Accept': 'text/plain', 'User-Agent':'OpenTTD-Python/0.0'})
+        response = self.getresponse()
+        if not response.status == httplib.OK:
+            raise _error.HTTPError("Error getting url from mirror server: %s" % response.reason)
+        data = response.read()
+        print response.getheaders()
+        return [url.strip() for url in data.split() if len(url.strip()) > 0]
 class ContentClient(client.Client):
     def __init__(self):
         client.Client.__init__(self, const.NETWORK_CONTENT_SERVER_HOST, const.NETWORK_CONTENT_SERVER_PORT)
@@ -23,7 +41,7 @@ class ContentClient(client.Client):
             return
         p = DataPacket(0, const.PACKET_CONTENT_CLIENT_INFO_LIST)
         p.send_uint8(type)
-        p.send_uint32(0 << 28 | 8 << 24 | 0 << 20 | 0 << 19 | 0)
+        p.send_uint32(version.generate_newgrf_version(1,1,0,False,0)) # XXX find better way?
         self.sendMsg_TCP(p.command, p.data)
     def ContentList_Request_ids(self, ids):
         count = len(ids)
@@ -36,8 +54,7 @@ class ContentClient(client.Client):
                 p.send_uint32(ids[i])
             self.sendMsg_TCP(p.command, p.data)
             count -= p_count
-            sent += p_count
-        
+            sent += p_count   
     def ContentList_Receive(self):
         self.socket_tcp.settimeout(1)
         while 1:
@@ -46,8 +63,7 @@ class ContentClient(client.Client):
             except socket.timeout:
                 break
             self.ContentList_Process(p=p)
-        self.socket_tcp.settimeout(600)
-        
+        self.socket_tcp.settimeout(600)     
     def ContentList_Process(self, data="", p=None):
         structz.decodeerror = 'ignore'
         ci = DataStorageClass()
@@ -75,14 +91,13 @@ class ContentClient(client.Client):
         
         self.ContentList.append(ci)
         structz.decodeerror = 'strict'
-
-    def ContentList_Get(self):
+    def ContentList_Get(self, id=const.ContentType['END']):
         self.ContentList = []
-        self.ContentList_Request(const.ContentType['END'])
+        self.ContentList_Request(id)
         self.ContentList_Receive()
     def ContentList_PrintEntry(self, entry):
         print '-' * 50
-        print '#%d  --  %s v%s' % (entry.id, entry.name, entry.version)
+        print '#%d  (%13s)--  %s v%s %d' % (entry.id, const.ContentTypeDescr[entry.type], entry.name, entry.version, entry.unique_id)
         print "%dkb - %s" % (entry.filesize / 1024, entry.url)
         print "%d dependencies:" % entry.dependency_count
         for dep in entry.dependencies:
@@ -91,12 +106,50 @@ class ContentClient(client.Client):
         print "md5sum:", entry.md5sum[0].encode('hex')
         print "-"
         print entry.description
-        
+import sys
+def usage():
+    print "OpenTTD Content Server Basic Script"
+    print "Usage: %s command [arguments]" % sys.argv[0]
+    print "Commands:"
+    print "    help -- prints help"
+    print "    list [contenttype] -- prints a list of entries"
+    print "    geturl id1 [id2] [id3] ... -- prints the url of an entry"
+    print "    getinfo id1 [id2] [id3] ... -- prints info about entries"
 if __name__ == '__main__':
-    c = ContentClient()
-    c.connect()
-    c.ContentList_Get()
-    c.disconnect()
-    for i in c.ContentList:
-        c.ContentList_PrintEntry(i)
-    
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit()
+    command = sys.argv[1].strip()
+    if command == 'help':
+        usage()
+        sys.exit()
+    elif command == 'list':
+        id = const.ContentType['END']
+        if len(sys.argv) > 2:
+            if sys.argv[2].strip() in const.ContentType:
+                id = const.ContentType[sys.argv[2].strip()]
+            else:
+                id = int(sys.argv[2])
+        c = ContentClient()
+        c.connect()
+        c.ContentList_Get(id)
+        c.disconnect()
+        print "%d files:" % len(c.ContentList)
+        for i in c.ContentList:
+            c.ContentList_PrintEntry(i)
+    elif command == 'geturl':
+        c = MirrorClient()
+        print c.ContentList_URL_Get(ids)
+        c.close()
+    elif command == 'getinfo':
+        if len(sys.argv) < 3:
+            sys.exit()
+        ids = [int(i) for i in sys.argv[2:]]
+        c = ContentClient()
+        c.connect()
+        c.ContentList = []
+        c.ContentList_Request_ids(ids)
+        c.ContentList_Receive()
+        c.disconnect()
+        for i in c.ContentList:
+            c.ContentList_PrintEntry(i)
